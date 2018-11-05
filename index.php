210f0146023f938bb831829d8af636e8a09f603b
julioCoutinho <juliocezar0002@gmail.com>
2018-11-05 12:06:42 -0200

    commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <!-- Index CSS -->
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-6" id="menuBox">
+                    <div class="row" id="rowLogoIfmg">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-5">
+                            <img src="img/logoIfmg.jpeg" class="img-fluid|thumbnail rounded-top|rounded-right|rounded-bottom|rounded-left|rounded-circle|" alt="" id="logoIfmg">
+                        </div>
+                        <div class="col-md-3"></div>
+                    </div>
+                    <div class="row" id="rowLogarMenu">
+                        <div class="col-md-3"></div>
+                        <div class="col-md-9">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-1"><img align="right" src="img/Icon/login-icon.png" class="img-fluid|thumbnail rounded-top|rounded-right|rounded-bottom|rounded-left|rounded-circle|" alt="" width="33px" heigth="33px"></div>
+                                    <div class="col-md-2"><label>Usuário:</label></div>
+                                    <div class="col-md-9"><input type="text" class="menuInputs"></div>
+                                </div>
+                                <div class="row" id="rowLogarPass">
+                                    <div class="col-md-1"></div>
+                                    <div class="col-md-2">Senha:</div>
+                                    <div class="col-md-9"><input type="password" class="menuInputs"></div>
+                                </div>
+                                <div class="row" id="rowLogarBotoes">
+                                    <div class="col-md-6" align="right" id="esqueciMinhaSenha"><a href="#">Esqueci minha senha</a></div>
+                                    <div class="col-md-1"></div>
+                                    <div class="col-md-2"align="right"><button type="button" id="btnEntrar"><a href="#">Entrar</a></button></div>
+                                    <div class="col-md-3"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+            
+        </div>
+
+
+        <!-- jQuery, popperjs e Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </body>
+</html>
